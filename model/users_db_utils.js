e358f17e40173543740d8233a393e5f6262e9def
amukherjee <Iasabhi123>
2017-04-05 02:06:53 -0500

    Updated index.html

--- model/users_db_utils.js
+++ model/users_db_utils.js
@@ -30,9 +30,7 @@ var findUserDb = function findUserDb(email_pswd_json, res) {
                 }
                 else if (typeof results[0] == 'undefined') {
                     console.log("user not found");
-                	res.redirect("/");    
-			//res.send({error:'Email or Password is incorrect!'});
-                    
+                	res.redirect("/", {error:'Email or Password is incorrect!'});
                 }
                 else {
                     console.log("User found");
