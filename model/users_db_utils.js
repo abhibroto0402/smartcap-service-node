9417b07006a23ffcd971a0a89c91ffaf50339169
amukherjee <Iasabhi123>
2017-04-05 03:38:46 -0500

    HTML

--- model/users_db_utils.js
+++ model/users_db_utils.js
@@ -34,7 +34,7 @@ var findUserWeb = function findUserDb(email_pswd_json, res, req) {
                 }
                 else {
                     console.log("User found");
-                    req.session.user = result[0];
+                    req.session.user = results[0];
                 }
             });
         }
--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.post('/login', function (req, res){
 
 app.get('/dashboard', function (req, res) {
    if(req.session &&  req.session.user){
-       res.send(req.session)
+       res.send(req.session.user);
    }
 });
 
