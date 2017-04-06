898dcc63ed14b08fe32acaad7c8280365f2ab749
amukherjee <Iasabhi123>
2017-04-06 15:52:06 -0500

    html in view updated

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,7 @@ var express = require ('express');
 var router = express.Router();
 
 router.get('/dashboard', function (req, res, next) {
-    res.render('./views/dashboard.html');
+    res.render('dashboard.html');
 });
 
 module.exports = router;
