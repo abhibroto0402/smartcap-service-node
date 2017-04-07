7f33af9635d2fb9a031eb0d94da39875ea3d7190
amukherjee <Iasabhi123>
2017-04-06 22:25:56 -0500

    added angular stuff

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1
+        }]
+    }
+});
